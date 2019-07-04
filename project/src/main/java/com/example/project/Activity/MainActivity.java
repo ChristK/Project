5426b0f14cb6ee3e90e7f50d2e2bf3cfc66108c0
DuChen <i6zi@qq.com>
2019-07-04 20:52:29 +0100

    完善了Fragment转换的一点问题

--- project/src/main/java/com/example/project/Activity/MainActivity.java
+++ project/src/main/java/com/example/project/Activity/MainActivity.java
@@ -15,10 +15,28 @@ import com.example.project.Fragment.UserFragment;
 import com.example.project.R;
 import com.example.project.Util.SharedPerencesUtil;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    /** main fragment */
+    public static final int PAGE_HOME = 0;
+    /** user fragment */
+    public static final int PAGE_USER = 1;
+
+    /**manage fragment*/
+    private HashMap<Integer,Fragment> fragments = new HashMap<>();
+
+    //Current activity's fragment control
+    private int fragmentContentId = R.id.fragment_content;
+
+    /** Set the default fragment */
+    private int currentTab;
+
 
     private ImageView home;
     private ImageView user;
+
     //private Button logout;
     private SharedPerencesUtil sp;
     @Override
@@ -26,9 +44,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //keep login
         sp=SharedPerencesUtil.getInstance(getApplicationContext());
+
         //init control
         init();
+
+        //init fragment
+        initFragment();
+
+        //default Fragment
+        defaultFragment();
+
 
         //logout
         /**
@@ -48,45 +75,70 @@ public class MainActivity extends AppCompatActivity {
         home.setOnClickListener(onClickListener);
         user.setOnClickListener(onClickListener);
 
+    }
 
+    private void initFragment() {
+        fragments.put(PAGE_HOME, new MainFragment());
+        fragments.put(PAGE_USER, new UserFragment());
+    }
+
+    private void defaultFragment() {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.add(fragmentContentId,fragments.get(PAGE_HOME));
+        currentTab = PAGE_HOME;
+        ft.commit();
     }
 
 
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            FragmentManager fm=getFragmentManager();
-            FragmentTransaction ft=fm.beginTransaction();
-            Fragment f=null;
-
-            switch (v.getId()) {
-
-                case R.id.homepage:
-                    f = new MainFragment();
-                    break;
-
-                case R.id.userpage:
-                    f = new UserFragment();
-                    break;
-
-                default:
-                    break;
-
-            }
-
-            ft.replace(R.id.main,f,"frag");
-           // ft.replace(R.id.fragment, f);
-            ft.commit();
-        }
-    };
 
     //init control method
     private void init() {
         home=(ImageView)findViewById(R.id.homepage);
         user=(ImageView)findViewById(R.id.userpage);
+
        // logout=(Button)findViewById(R.id.button);
     }
 
 
+    private void changeTab(int page) {
+        if (currentTab == page) {
+            return;
+        }
+
+        //获取fragment的页码
+        Fragment fragment = fragments.get(page);
+        //fragment事务
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        //如果该Fragment对象被添加到了它的Activity中，那么它返回true，否则返回false。
+        //当前activity中添加的不是这个fragment
+        if(!fragment.isAdded()){
+            //所以将他加进去
+            ft.add(fragmentContentId,fragment);
+        }
+        //隐藏当前currentTab的
+        ft.hide(fragments.get(currentTab));
+        //显示现在page的
+        ft.show(fragments.get(page));
+        //当前显示的赋值给currentTab
+        currentTab = page;
+        //activity被销毁？  ！否
+        if (!this.isFinishing()) {
+            //允许状态丢失
+            ft.commitAllowingStateLoss();
+        }
+    }
+
+    View.OnClickListener onClickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            {
+                int temdId = v.getId();
+                if(temdId == R.id.homepage){
+                    changeTab(PAGE_HOME);
+                } else if (temdId == R.id.userpage) {
+                    changeTab(PAGE_USER);
+                }
+            }
+        }
+    };
 }
